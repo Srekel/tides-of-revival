c723c0de32b53cc2d020b8b615c598cce7357293
Anders Elfgren <srekel@gmail.com>
2022-09-30 15:44:31 +0200

    Git: Update sync_external.py to correct targets

--- sync_external.py
+++ sync_external.py
@@ -17,11 +17,11 @@ def sync_lib(folder, git_path, commit_sha):
 
 
 os.chdir("external")
-sync_lib("zig-args", "https://github.com/MasterQ32/zig-args.git", "1ff417ac1f31f8dbee3a31e5973b46286d42e71d")
+sync_lib("zig-args", "https://github.com/MasterQ32/zig-args.git", "master")
 # sync_lib("zig-flecs", "https://github.com/prime31/zig-flecs.git")
-sync_lib("zig-flecs", "https://github.com/Srekel/zig-flecs.git", "8a013623e3cf2c0e7884a3185bcb8d1670ec9b70")
-sync_lib("zig-gamedev", "https://github.com/michal-z/zig-gamedev.git", "9dd09272ee86509acd2c66f349cfb795fdb0e904")
-sync_lib("zigimg", "https://github.com/zigimg/zigimg.git", "a224b3c942d48dbf580a75f2f87de16046cb335f")
+sync_lib("zig-flecs", "https://github.com/Srekel/zig-flecs.git", "0cd280d9cc254b3e3634b90ea6d749a89b4c9610")
+sync_lib("zig-gamedev", "https://github.com/michal-z/zig-gamedev.git", "d95d8b08f5744daf1c4b6ce9ea0038640bed04be")
+sync_lib("zigimg", "https://github.com/zigimg/zigimg.git", "fff6ea92a00c5f6092b896d754a932b8b88149ff")
 # sync_lib("zls", "https://github.com/zigtools/zls.git", "949e4fe525abaf25699b7f15368ecdc49fd8b786")
 os.chdir("..")
 
