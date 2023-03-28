341bf42a243ec400081e54567b3e1f60141e9de2
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2023-03-28 20:49:16 +0200

    Build: point to the latest zig-gamedev in our fork

--- sync_external.py
+++ sync_external.py
@@ -23,7 +23,7 @@ if not os.path.isdir(external_dir):
 os.chdir(external_dir)
 sync_lib("websocket.zig", "https://github.com/karlseguin/websocket.zig.git", "b5ccdb1bafe6b3f59f84f275fb82603265bedc88")
 sync_lib("zig-args", "https://github.com/MasterQ32/zig-args.git", "c84f9709405b31ef3e72ab26b20ba3c37826f8ec")
-sync_lib("zig-gamedev", "https://github.com/Srekel/zig-gamedev.git", "b332a64052376dc7150d381cc459484b6794552e")
+sync_lib("zig-gamedev", "https://github.com/Srekel/zig-gamedev.git", "df25329d65870585d7992532cc64a382e2bcb822")
 sync_lib("zig-flecs", "https://github.com/Srekel/zig-flecs.git", "fe1f47d99ebd7495b16e4e56011426cd408138dc")
 sync_lib("zigimg", "https://github.com/zigimg/zigimg.git", "6d0f7d71a49b19564cf70f07577670f712cfc353")
 # sync_lib("zls", "https://github.com/zigtools/zls.git", "949e4fe525abaf25699b7f15368ecdc49fd8b786")
