36c8a0544df1739f8eb40c604deea191f52298bf
Anders Elfgren <srekel@gmail.com>
2023-06-19 20:22:11 +0200

    Gameplay: Spiders now rotate towards the player.

--- src/fsm/creature/state_spider.zig
+++ src/fsm/creature/state_spider.zig
@@ -36,12 +36,12 @@ fn updateMovement(pos: *fd.Position, rot: *fd.EulerRotation, fwd: *fd.Forward, d
     var self_pos_z = zm.loadArr3(pos.elems().*);
     const vec_to_player = player_pos_z - self_pos_z;
     const dir_to_player = zm.normalize3(vec_to_player);
-    self_pos_z += dir_to_player * dt * zm.f32x4s(0.2);
+    self_pos_z += dir_to_player * dt * zm.f32x4s(0.1);
 
     zm.store(pos.elems()[0..], self_pos_z, 3);
 }
 
-fn updateSnapToTerrain(physics_world: *zphy.PhysicsSystem, pos: *fd.Position, body: *fd.PhysicsBody) void {
+fn updateSnapToTerrain(physics_world: *zphy.PhysicsSystem, pos: *fd.Position, body: *fd.PhysicsBody, player_pos: *const fd.Position) void {
     const query = physics_world.getNarrowPhaseQuery();
 
     const ray_origin = [_]f32{ pos.x, pos.y + 200, pos.z, 0 };
@@ -60,14 +60,13 @@ fn updateSnapToTerrain(physics_world: *zphy.PhysicsSystem, pos: *fd.Position, bo
     if (result.has_hit) {
         pos.y = ray_origin[1] + ray_dir[1] * result.hit.fraction;
 
+        const up_z  = zm.f32x4(0, 1,0,0);
         const bodies = physics_world.getBodiesUnsafe();
         const body_self = zphy.tryGetBody(bodies, body.body_id).?;
         const body_hit = zphy.tryGetBody(bodies, result.hit.body_id).?;
-        const body_interface = physics_world.getBodyInterfaceMut();
-        const hit_normal = body_hit.getWorldSpaceSurfaceNormal(result.hit.sub_shape_id, ray.getPointOnRay(result.hit.fraction));
-        const hit_normal_z = zm.loadArr3(hit_normal);
-        const rot_wanted_z = blk: {
-            const up_z  = zm.f32x4(0, 1,0,0);
+        const rot_slope_z = blk: {
+            const hit_normal = body_hit.getWorldSpaceSurfaceNormal(result.hit.sub_shape_id, ray.getPointOnRay(result.hit.fraction));
+            const hit_normal_z = zm.loadArr3(hit_normal);
             if (hit_normal[1] < 0.99) { // TODO: Find a good value, this was just arbitrarily picked :)
                 const rot_axis_z = zm.cross3(up_z, hit_normal_z);
                 const dot = zm.dot3(up_z, hit_normal_z)[0];
@@ -78,6 +77,15 @@ fn updateSnapToTerrain(physics_world: *zphy.PhysicsSystem, pos: *fd.Position, bo
                  break :blk zm.quatFromAxisAngle(up_z, 0);
             }
         };
+
+        const player_pos_z = zm.loadArr3(player_pos.elemsConst().*);
+        const self_pos_z = zm.loadArr3(pos.elems().*);
+        const vec_to_player = player_pos_z - self_pos_z;
+        const dir_to_player = zm.normalize3(vec_to_player);
+        const angle_to_player = std.math.atan2(f32, -dir_to_player[2], dir_to_player[0]);
+        const rot_towards_player_z = zm.quatFromAxisAngle(up_z, angle_to_player + std.math.pi * 0.5);
+
+        const rot_wanted_z = zm.qmul(rot_towards_player_z,rot_slope_z);
         const rot_curr_z = zm.loadArr4(body_self.getRotation());
         const rot_new_z = zm.slerp(rot_curr_z, rot_wanted_z, 0.01); // TODO SmoothDamp
         const rot_new_normalized_z = zm.normalize4(rot_new_z);
@@ -85,6 +93,7 @@ fn updateSnapToTerrain(physics_world: *zphy.PhysicsSystem, pos: *fd.Position, bo
         zm.storeArr4(&rot, rot_new_normalized_z);
 
         // NOTE: Should this use MoveKinematic?
+        const body_interface = physics_world.getBodyInterfaceMut();
         body_interface.setPositionRotationAndVelocity(
             body.body_id,
             pos.elems().*,
@@ -141,7 +150,7 @@ fn update(ctx: fsm.StateFuncContext) void {
         const pos_before = comps.pos.*;
         _ = pos_before;
         updateMovement(comps.pos, comps.rot, comps.fwd, ctx.dt, player_pos);
-        updateSnapToTerrain(ctx.physics_world, comps.pos, comps.body);
+        updateSnapToTerrain(ctx.physics_world, comps.pos, comps.body, player_pos);
     }
 }
 
