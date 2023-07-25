36e86f0670a55d930fdddb7a756b9dec0dd52c02
Anders Elfgren <srekel@gmail.com>
2023-07-25 20:06:48 +0200

    Code: Moved zig-flecs original license into flecs_util.zig

--- src/flecs_util/flecs_util.zig
+++ src/flecs_util/flecs_util.zig
@@ -16,34 +16,34 @@ pub const meta = @import("meta.zig");
 
 pub const ECS_HI_COMPONENT_ID = 256;
 
-// Builtin pipeline tags
-// pub const Phase = enum(ecs.id_t) {
-//     monitor = ecs.Monitor,
-//     // pipeline = ecs.Pipeline,
-//     pre_frame = ecs.PreFrame,
-//     on_load = ecs.OnLoad,
-//     post_load = ecs.PostLoad,
-//     pre_update = ecs.PreUpdate,
-//     on_update = ecs.OnUpdate,
-//     on_validate = ecs.OnValidate,
-//     post_update = ecs.PostUpdate,
-//     pre_store = ecs.PreStore,
-//     on_store = ecs.OnStore,
-//     post_frame = ecs.PostFrame,
-// };
+// NOTE(Anders): This folder is essentially a copy of prime31's original bindings with various
+// changes to make it suit Tides of Revival and to make it be a utility layer on top of the
+// zig-gamedev bindings.
+// https://github.com/prime31/zig-flecs
+//
+// I consider my changes to this to be in the Public Domain but the original code is MIT.
+// (onus is on you to figure out which is which and also to do what you want with this :) )
+//
+// Prime31's original license follows:
 
-// pub const Event = enum(ecs.id_t) {
-//     // Event. Triggers when an id (component, tag, pair) is added to an entity
-//     on_add = ecs.OnAdd,
-//     // Event. Triggers when an id (component, tag, pair) is removed from an entity
-//     on_remove = ecs.OnRemove,
-//     // Event. Triggers when a component is set for an entity
-//     on_set = ecs.OnSet,
-//     // Event. Triggers when a component is unset for an entity
-//     un_set = ecs.UnSet,
-//     // Event. Triggers when an entity is deleted.
-//     on_delete = ecs.OnDelete,
-//     // Event. Exactly-once trigger for when an entity matches/unmatches a filter
-//     monitor = ecs.Monitor,
-//     _,
-// };
+// MIT License
+
+// Copyright (c) 2022 Colton Franklin
+
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
