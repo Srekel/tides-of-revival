9175b5d11f1a3d61c75553692bddc26c337ad122
Anders Elfgren <srekel@gmail.com>
2023-07-25 20:28:19 +0200

    Code: Updated the license for flecs_util.

--- src/flecs_util/flecs_util.zig
+++ src/flecs_util/flecs_util.zig
@@ -21,29 +21,7 @@ pub const ECS_HI_COMPONENT_ID = 256;
 // zig-gamedev bindings.
 // https://github.com/prime31/zig-flecs
 //
-// I consider my changes to this to be in the Public Domain but the original code is MIT.
-// (onus is on you to figure out which is which and also to do what you want with this :) )
+// 💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛
+// With his blessing, all this code is now under the same license as Tides of Revival.
+// 💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛💜🧡💗💚💛
 //
-// Prime31's original license follows:
-
-// MIT License
-
-// Copyright (c) 2022 Colton Franklin
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-// SOFTWARE.
