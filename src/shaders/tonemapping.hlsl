07ff2f63f39eb58be56160d008c7b432b27df725
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2023-09-20 07:37:40 +0200

    Rendering: better ACES tonemapping

--- src/shaders/tonemapping.hlsl
+++ src/shaders/tonemapping.hlsl
@@ -14,53 +14,21 @@ struct DrawConst {
 
 ConstantBuffer<DrawConst> cbv_draw_const : register(b0);
 
-float3 reinhard(float3 hdr, float k = 1.0f) { return hdr / (hdr + k); }
+//
+// Simpler approximation to ACES, by Krzysztof Narkowicz.
+// https://knarkowicz.wordpress.com/2016/01/06/aces-filmic-tone-mapping-curve/
+//
+float3 acesApprox(float3 color) {
+  // Obtain original ACES curve by multiplying the input HRD by 0.6
+  color *= 0.6;
 
-//==========================================================================================
-// ACES
-//==========================================================================================
+  float a = 2.51;
+  float b = 0.03;
+  float c = 2.43;
+  float d = 0.59;
+  float e = 0.14;
 
-//  Baking Lab
-//  by MJP and David Neubelt
-//  http://mynameismjp.wordpress.com/
-//  All code licensed under the MIT license
-
-// sRGB => XYZ => D65_2_D60 => AP1 => RRT_SAT
-static const float3x3 aces_mat_input =
-{
-    {0.59719, 0.35458, 0.04823},
-    {0.07600, 0.90834, 0.01566},
-    {0.02840, 0.13383, 0.83777}
-};
-
-// ODT_SAT => XYZ => D60_2_D65 => sRGB
-static const float3x3 aces_mat_output =
-{
-    { 1.60475, -0.53108, -0.07367},
-    {-0.10208,  1.10813, -0.00605},
-    {-0.00327, -0.07276,  1.07602}
-};
-
-float3 RRTAndODTFit(float3 v)
-{
-    float3 a = v * (v + 0.0245786f) - 0.000090537f;
-    float3 b = v * (0.983729f * v + 0.4329510f) + 0.238081f;
-    return a / b;
-}
-
-float3 aces(float3 color)
-{
-    color = mul(aces_mat_input, color);
-
-    // Apply RRT and ODT
-    color = RRTAndODTFit(color);
-
-    color = mul(aces_mat_output, color);
-
-    // Clamp to [0, 1]
-    color = saturate(color);
-
-    return color;
+  return saturate((color * (a * color + b)) / (color * (c * color + d) + e));
 }
 
 [RootSignature(ROOT_SIGNATURE)]
@@ -80,15 +48,6 @@ float4 psTonemapping(FullscreenTriangleOutput input) : SV_Target
     Texture2D hdr_texture = ResourceDescriptorHeap[cbv_draw_const.hdr_texture_index];
 
     float3 hdr_sample = hdr_texture.Sample(sam_aniso_clamp, input.uv).rgb;
-    float3 color = hdr_sample;
-    if (cbv_draw_const.tonemapping_function == 0)
-    {
-        color = aces(color);
-    }
-    else if (cbv_draw_const.tonemapping_function == 1)
-    {
-        color = reinhard(color);
-    }
-
+    float3 color = acesApprox(hdr_sample);
     return float4(gamma(color), 1.0);
 }
