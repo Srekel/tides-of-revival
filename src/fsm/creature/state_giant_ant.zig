51aa9c2b1dfafc45c35ee5967f2478ac57e448ae
Anders Elfgren <srekel@gmail.com>
2023-11-11 19:26:44 +0100

    Gameplay: Skittery ants

--- src/fsm/creature/state_giant_ant.zig
+++ src/fsm/creature/state_giant_ant.zig
@@ -31,13 +31,13 @@ pub const NonMovingBroadPhaseLayerFilter = extern struct {
 };
 
 fn updateMovement(pos: *fd.Position, rot: *fd.Rotation, fwd: *fd.Forward, dt: zm.F32x4, player_pos: *const fd.Position) void {
-    _ = fwd;
     _ = rot;
     const player_pos_z = zm.loadArr3(player_pos.elemsConst().*);
     var self_pos_z = zm.loadArr3(pos.elems().*);
     const vec_to_player = player_pos_z - self_pos_z;
     const dir_to_player = zm.normalize3(vec_to_player);
-    self_pos_z += dir_to_player * dt * zm.f32x4s(0.1);
+    _ = dir_to_player;
+    self_pos_z += fwd.asZM() * dt * zm.f32x4s(2);
 
     zm.store(pos.elems()[0..], self_pos_z, 3);
 }
@@ -88,14 +88,18 @@ fn updateSnapToTerrain(physics_world: *zphy.PhysicsSystem, pos: *fd.Position, bo
         const player_pos_z = zm.loadArr3(player_pos.elemsConst().*);
         const self_pos_z = zm.loadArr3(pos.elems().*);
         const vec_to_player = player_pos_z - self_pos_z;
-        if (zm.length3(vec_to_player)[0] > 1) {
+        const dist_to_player_sq = zm.lengthSq3(vec_to_player)[0];
+        if (dist_to_player_sq > 1) {
+            const skitter = dist_to_player_sq > (15 * 15) and std.math.modf(player_pos.y + pos.y * 0.25).fpart > 0.25;
             const dir_to_player = zm.normalize3(vec_to_player);
-            const angle_to_player = std.math.atan2(f32, dir_to_player[0], dir_to_player[2]);
+            const skitter_angle_offset: f32 = if (skitter) std.math.modf(player_pos.y + pos.y * 0.25).fpart * 2 - 1 else 0;
+            const angle_to_player = std.math.atan2(f32, dir_to_player[0], dir_to_player[2]) + skitter_angle_offset;
             const rot_towards_player_z = zm.quatFromAxisAngle(up_z, angle_to_player + handedness_offset);
 
             const rot_wanted_z = zm.qmul(rot_towards_player_z, rot_slope_z);
             const rot_curr_z = zm.loadArr4(body_self.getRotation());
-            const rot_new_z = zm.slerp(rot_curr_z, rot_wanted_z, 0.01); // TODO SmoothDamp
+            const slerp_factor: f32 = if (skitter) 0.01 else 0.01;
+            const rot_new_z = zm.slerp(rot_curr_z, rot_wanted_z, slerp_factor); // TODO SmoothDamp
             const rot_new_normalized_z = zm.normalize4(rot_new_z);
             var rot: [4]f32 = undefined;
             zm.storeArr4(&rot, rot_new_normalized_z);
@@ -153,6 +157,8 @@ fn update(ctx: fsm.StateFuncContext) void {
     const player_ent = ecs.lookup(ctx.ecsu_world.world, "main_player");
     const player_pos = ecs.get(ctx.ecsu_world.world, player_ent, fd.Position).?;
     const body_interface = ctx.physics_world.getBodyInterfaceMut();
+
+    std.log.info("ant dt: {d:5.4}", .{ctx.dt[0]});
 
     while (entity_iter.next()) |comps| {
         if (entity_iter.entity() == player_ent) {
