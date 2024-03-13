d84f9ec73c467a25374324002b21e4b60248097b
Giuseppe Modarelli <giuseppe.modarelli@gmail.com>
2024-03-13 16:40:02 +0100

    Initialize memory tracking and logging

--- src/zforge_main.zig
+++ src/zforge_main.zig
@@ -1,9 +1,17 @@
 const std = @import("std");
 const zforge = @import("zforge");
+
 const FileSystem = zforge.FileSystem;
 const Graphics = zforge.Graphics;
+const Log = zforge.Log;
+const Memory = zforge.Memory;
 
 const window = @import("renderer/window.zig");
+
+const RendererContext = struct {
+    renderer: [*c]Graphics.Renderer = null,
+    graphics_queue: [*c]Graphics.Queue = null,
+};
 
 pub fn main() void {
     std.log.info("Did we make it? {}", .{zforge});
@@ -12,6 +20,11 @@ pub fn main() void {
     defer window.deinit();
     const main_window = window.createWindow("Tides of Revival: A Fort Wasn't Built In A Day") catch unreachable;
     _ = main_window;
+
+    if (!Memory.initMemAlloc("Tides Renderer")) {
+        std.log.err("Failed to initialize Z-Forge Memory System", .{});
+        return;
+    }
 
     var fs_desc: FileSystem.FileSystemInitDesc = undefined;
     fs_desc.pAppName = "Tides Renderer";
@@ -25,19 +38,36 @@ pub fn main() void {
     }
     defer FileSystem.exitFileSystem();
 
+    FileSystem.fsSetPathForResourceDir(FileSystem.fsGetSystemFileIO(), FileSystem.ResourceMount.RM_DEBUG, FileSystem.ResourceDirectory.RD_LOG, "");
+
+    Log.init_log("Tides Renderer", Log.LogLevel.eALL);
+    defer Log.exit_log();
+
     FileSystem.fsSetPathForResourceDir(FileSystem.fsGetSystemFileIO(), FileSystem.ResourceMount.RM_CONTENT, FileSystem.ResourceDirectory.RD_GPU_CONFIG, "GPUCfg");
     FileSystem.fsSetPathForResourceDir(FileSystem.fsGetSystemFileIO(), FileSystem.ResourceMount.RM_CONTENT, FileSystem.ResourceDirectory.RD_SHADER_BINARIES, "content/compiled_shaders");
     FileSystem.fsSetPathForResourceDir(FileSystem.fsGetSystemFileIO(), FileSystem.ResourceMount.RM_CONTENT, FileSystem.ResourceDirectory.RD_TEXTURES, "content");
     FileSystem.fsSetPathForResourceDir(FileSystem.fsGetSystemFileIO(), FileSystem.ResourceMount.RM_CONTENT, FileSystem.ResourceDirectory.RD_MESHES, "content");
     FileSystem.fsSetPathForResourceDir(FileSystem.fsGetSystemFileIO(), FileSystem.ResourceMount.RM_CONTENT, FileSystem.ResourceDirectory.RD_FONTS, "content");
 
+    var renderer_context = RendererContext{};
+
     var renderer_desc = std.mem.zeroes(Graphics.RendererDesc);
     renderer_desc.mD3D11Supported = false;
     renderer_desc.mGLESSupported = false;
     renderer_desc.mShaderTarget = Graphics.ShaderTarget.SHADER_TARGET_6_6;
     renderer_desc.mDisableReloadServer = true;
-    var renderer: [*c]Graphics.Renderer = null;
-    Graphics.initRenderer("Tides Renderer", &renderer_desc, &renderer);
+    Graphics.initRenderer("Tides Renderer", &renderer_desc, &renderer_context.renderer);
+    if (renderer_context.renderer == null) {
+        std.log.err("Failed to initialize Z-Forge Renderer", .{});
+        return;
+    }
+    defer Graphics.exitRenderer(renderer_context.renderer);
+
+    var queue_desc = std.mem.zeroes(Graphics.QueueDesc);
+    queue_desc.mType = Graphics.QueueType.QUEUE_TYPE_GRAPHICS;
+    queue_desc.mFlag = Graphics.QueueFlag.QUEUE_FLAG_INIT_MICROPROFILE;
+    Graphics.add_queue(renderer_context.renderer, &queue_desc, &renderer_context.graphics_queue);
+    defer Graphics.remove_queue(renderer_context.renderer, renderer_context.graphics_queue);
 
     var is_running = true;
     while (is_running) {
