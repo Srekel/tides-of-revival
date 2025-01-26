c5263aadb6f065317b36eb136350fd7bccbeaae1
Anders Elfgren <srekel@gmail.com>
2025-01-26 09:09:47 +0100

    Build: Bunch of improvements to make full_pull do everything needed for initial run.

--- full_pull.py
+++ full_pull.py
@@ -3,30 +3,46 @@ import sys
 import shutil
 import subprocess
 import sync_external
-import platform
-import filecmp
-import argparse
 
 from pathlib import Path
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKCYAN = "\033[96m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = "\033[91m"
+    ENDC = "\033[0m"
+    BOLD = "\033[1m"
+    UNDERLINE = "\033[4m"
 
 
 def do_task(text, task_func, skip_confirm=False):
     print("")
     print("")
-    print("#" * len(text))
-    print(text)
+    print(bcolors.OKCYAN + "#" * len(text) + bcolors.ENDC)
+    print(bcolors.OKCYAN + text + bcolors.ENDC)
     if not skip_confirm:
-        print("Press enter...")
+        print(bcolors.OKGREEN + "Press enter..." + bcolors.ENDC)
         input()
     task_func()
 
+
+script_path = os.path.dirname(os.path.realpath(__file__))
+tools_path = os.path.join(script_path, "tools")
+tools_binaries_path = os.path.join(tools_path, "binaries")
+zig_path = os.path.join(tools_binaries_path, "zigup", "zig.exe")
+simulator_root = os.path.join(tools_path, "simulator")
+simulator_path = os.path.join(simulator_root, "zig-out", "bin", "Simulator.exe")
+asset_cooker_path = os.path.join(tools_binaries_path, "AssetCooker", "AssetCooker.exe")
 
 # TASKS
 
 
 def task_sync_build_tools():
-    script_path = os.path.dirname(os.path.realpath(__file__))
-    build_tools_path = os.path.join(script_path, "tools", "external", "msvc_BuildTools")
+    build_tools_path = os.path.join("C:", os.sep, "msvc_BuildTools")
     if not os.path.isdir(build_tools_path):
         component_ids = "Microsoft.VisualStudio.Component.VC.CoreBuildTools Microsoft.VisualStudio.Component.VC.CoreIde Microsoft.VisualStudio.Component.VC.Redist.14.Latest Microsoft.VisualStudio.Component.VC.Tools.x86.x64"
         subprocess.run(
@@ -49,28 +65,55 @@ def task_sync_external():
     sync_external.main()
 
 
-def task_copy_zig():
-    pass
-
-
 def task_sync_svn():
+    if not os.path.exists(os.path.join(script_path, ".svn")):
+        print("You need to download, install, and clone SVN.")
+        print("Then restart thiss script.")
+        print("Note: If you don't have a user/pw, poke Anders. (Srekel)")
+        input()
+        sys.exit(0)
+
     os.system("svn update")
 
 
+def task_start_asset_cooker():
+    os.startfile(asset_cooker_path)
+
+
 def task_build_game():
-    Path(os.path.join("zig-out", "bin", "content", "systems")).mkdir(
-        parents=True, exist_ok=True
-    )
+    # Path(os.path.join("zig-out", "bin", "content", "systems")).mkdir(
+    #     parents=True, exist_ok=True
+    # )
     # os.system("zig build")
+    print(zig_path)
     subprocess.run(
         [
-            "zig",
+            zig_path,
             "build",
             "-Dtarget=native-native-msvc",
             "--summary",
             "failures",
         ],
-        cwd=".",
+        cwd=script_path,
+        capture_output=False,
+        text=True,
+    )
+
+
+def task_build_simulator():
+    # Path(os.path.join("zig-out", "bin", "content", "systems")).mkdir(
+    #     parents=True, exist_ok=True
+    # )
+    # os.system("zig build")
+    subprocess.run(
+        [
+            zig_path,
+            "build",
+            # "-Dtarget=native-native-msvc",
+            "--summary",
+            "failures",
+        ],
+        cwd=simulator_root,
         capture_output=False,
         text=True,
     )
@@ -78,7 +121,7 @@ def task_build_game():
 
 def task_nuke_cache():
     if os.path.isdir("zig-cache"):
-        shutil.rmtree("zig-cache")
+        shutil.rmtree(".zig-cache")
 
 
 def task_nuke_old_world():
@@ -88,75 +131,21 @@ def task_nuke_old_world():
 
 
 def task_generate_new_world():
-    path = os.path.join("zig-out", "bin", "TidesOfRevival.exe")
-    os.system(path + " --offlinegen")
+    os.startfile(simulator_path, cwd=os.path.dirname(simulator_path))
 
 
-def task_sync_world():
-    if platform.system() == "Windows":
-        subprocess.run(
-            [
-                "robocopy",
-                os.path.join("content", "patch"),
-                os.path.join("zig-out", "bin", "content", "patch"),
-                "/MIR",
-                "/MT:4",
-            ],
-            cwd=".",
-            capture_output=False,
-            text=True,
-        )
-    else:
-        # https://stackoverflow.com/questions/22493492/compare-directories-delete-leftover-files-copy-new-ones
-        src = os.path.join("content", "patch")
-        dst = os.path.join("zig-out", "bin", "content", "patch")
-        for src_root, src_dirs, src_files in os.walk(src, topdown=True):
-            dst_root = os.path.join(dst, os.path.relpath(src_root, src))
-            dirs = filecmp.dircmp(src_root, dst_root)
-            for item in dirs.right_only:
-                print("Removing " + item)
-                dst_path = os.path.join(dst_root, item)
-                if os.path.isdir(dst_path):
-                    shutil.rmtree(dst_path)
-                else:
-                    os.remove(dst_path)
-            for item in dirs.left_only:
-                print("Adding " + item)
-                src_path = os.path.join(src_root, item)
-                if os.path.isdir(src_path):
-                    shutil.copytree(src_path, os.path.join(dst_root, item))
-                else:
-                    shutil.copy2(src_path, os.path.join(dst_root, item))
+print("")
+print("Performing: Full Pull")
+print("")
 
-
-parser = argparse.ArgumentParser(prog="full_pull")
-
-group = parser.add_mutually_exclusive_group(required=True)
-group.add_argument("-f", "--full", action="store_true")
-group.add_argument("-g", "--generate-world", action="store_true")
-
-args = parser.parse_args()
-
-if args.full:
-    print("")
-    print("Performing: Full Pull")
-    print("")
-
-    do_task("Pulling Git...", task_sync_git)
-    do_task("Syncing external libs and zig.exe...", task_sync_external)
-    do_task("You need to copy zig!", task_copy_zig)
-    do_task("Acquiring MSVC BuildTools!", task_sync_build_tools)
-    do_task("Syncing SVN...", task_sync_svn)
-    do_task("Nuking cache...", task_nuke_cache)
-    do_task("Building game...", task_build_game)
-    do_task("Generating game world...", task_generate_new_world)
-elif args.generate_world:
-    print("")
-    print("Performing: Generate World")
-    print("")
-
-    do_task("Building game...", task_build_game, True)
-    do_task("Generating game world...", task_generate_new_world, True)
+do_task("Pulling Git...", task_sync_git)
+do_task("Syncing external libs and zig.exe...", task_sync_external)
+do_task("Acquiring MSVC BuildTools!", task_sync_build_tools)
+do_task("Syncing SVN...", task_sync_svn)
+do_task("Building game...", task_build_game)
+do_task("Building simulator...", task_build_simulator)
+do_task("Starting asset cooker...", task_start_asset_cooker)
+do_task("Generating game world...", task_generate_new_world)
 
 print("")
 print("")
