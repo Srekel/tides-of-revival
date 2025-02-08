6230324695834c29cab542b6767981d22cec15e3
Giuseppe Modarelli <hello@gmodarelli.com>
2025-02-08 08:34:23 +0100

    Asset Compiler: correct executable name in build.zig

--- tools/asset_compiler/build.zig
+++ tools/asset_compiler/build.zig
@@ -6,7 +6,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "AssetCooker",
+        .name = "AssetCompiler",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
