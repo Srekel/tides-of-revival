7667f99d04b49335ca00d54d54c2a6f350ce6fb3
Giuseppe Modarelli <hello@gmodarelli.com>
2025-02-09 08:41:48 +0100

    Asset Compiler: various fixed and generate metadata functionality

--- tools/asset_compiler/src/main.zig
+++ tools/asset_compiler/src/main.zig
@@ -5,6 +5,7 @@ const TextureFormat = enum {
     BC1_UNORM,
     BC1_UNORM_SRGB,
     BC3_UNORM,
+    BC4_UNORM,
     BC5_UNORM,
 };
 
@@ -23,6 +24,7 @@ pub fn main() !void {
     const parsed_args = args.parseForCurrentProcess(struct {
         input: []const u8 = "",
         output: []const u8 = "",
+        @"generate-metadata": bool = false,
 
         pub const shorthands = .{
             .i = "input",
@@ -35,8 +37,11 @@ pub fn main() !void {
     defer arena_state.deinit();
     const arena = arena_state.allocator();
 
-    std.log.debug("--input {s}", .{parsed_args.options.input});
-    std.log.debug("--output {s}", .{parsed_args.options.output});
+    if (parsed_args.options.@"generate-metadata") {
+        std.log.debug("Generate metadata", .{});
+        try generateMetadata(arena);
+        return;
+    }
 
     var file = try std.fs.openFileAbsolute(parsed_args.options.input, .{});
     defer file.close();
@@ -54,7 +59,6 @@ pub fn main() !void {
 
         if (std.mem.eql(u8, tag, "version")) {
             const version = splits.next().?;
-            std.log.debug("Version: '{s}'", .{version[1..]});
             std.debug.assert(std.mem.eql(u8, version[1..], "1"));
             continue;
         }
@@ -62,7 +66,6 @@ pub fn main() !void {
         if (std.mem.eql(u8, tag, "source")) {
             const source = splits.next().?;
             texture_info.source_path = try arena.dupe(u8, source[1..]);
-            std.log.debug("Source Path: '{s}'", .{texture_info.source_path});
             continue;
         }
 
@@ -74,34 +77,32 @@ pub fn main() !void {
                 texture_info.format = .BC1_UNORM_SRGB;
             } else if (std.mem.eql(u8, format[1..], "BC3_UNORM")) {
                 texture_info.format = .BC3_UNORM;
+            } else if (std.mem.eql(u8, format[1..], "BC4_UNORM")) {
+                texture_info.format = .BC4_UNORM;
             } else if (std.mem.eql(u8, format[1..], "BC5_UNORM")) {
                 texture_info.format = .BC5_UNORM;
             } else {
                 std.debug.panic("Unsupported format '{s}'", .{format[1..]});
             }
 
-            std.log.debug("Format: '{}'", .{texture_info.format});
             continue;
         }
 
         if (std.mem.eql(u8, tag, "width")) {
             const width = splits.next().?;
             texture_info.width = try std.fmt.parseInt(u32, width[1..], 10);
-            std.log.debug("Width: '{d}'", .{texture_info.width.?});
             continue;
         }
 
         if (std.mem.eql(u8, tag, "height")) {
             const height = splits.next().?;
             texture_info.height = try std.fmt.parseInt(u32, height[1..], 10);
-            std.log.debug("Height: '{d}'", .{texture_info.height.?});
             continue;
         }
 
         if (std.mem.eql(u8, tag, "mip_count")) {
             const mip_count = splits.next().?;
             texture_info.mip_count = try std.fmt.parseInt(u32, mip_count[1..], 10);
-            std.log.debug("Mip Count: '{d}'", .{texture_info.mip_count.?});
             continue;
         }
 
@@ -113,7 +114,6 @@ pub fn main() !void {
                 texture_info.srgb = false;
             }
 
-            std.log.debug("sRGB: '{}'", .{texture_info.srgb.?});
             continue;
         }
 
@@ -125,7 +125,6 @@ pub fn main() !void {
                 texture_info.invert_y = false;
             }
 
-            std.log.debug("Invert Y: '{}'", .{texture_info.invert_y.?});
             continue;
         }
     }
@@ -171,6 +170,9 @@ fn executeTextureConversionV1(desc: *TextureInfoV1, arena: std.mem.Allocator) !v
         },
         .BC3_UNORM => {
             try argv.append("BC3_UNORM");
+        },
+        .BC4_UNORM => {
+            try argv.append("BC4_UNORM");
         },
         .BC5_UNORM => {
             try argv.append("BC5_UNORM");
@@ -220,7 +222,7 @@ fn executeTextureConversionV1(desc: *TextureInfoV1, arena: std.mem.Allocator) !v
 
     // Specify the source image file path
     var source_path_buffer: [1024]u8 = undefined;
-    const source_absolute_path = try std.fmt.bufPrint(&source_path_buffer, "{s}/../../../{s}", .{cwd_absolute, desc.source_path});
+    const source_absolute_path = try std.fmt.bufPrint(&source_path_buffer, "{s}/../../../content/{s}", .{cwd_absolute, desc.source_path});
     try argv.append(source_absolute_path);
 
     var cmd = std.process.Child.init(@ptrCast(argv.items), std.heap.page_allocator);
@@ -228,4 +230,104 @@ fn executeTextureConversionV1(desc: *TextureInfoV1, arena: std.mem.Allocator) !v
 
     const term = try cmd.wait();
     std.debug.assert(term == .Exited);
+}
+
+fn generateMetadata(arena: std.mem.Allocator) !void {
+    var content_dir = try std.fs.cwd().openDir("../../../content", .{.iterate = true, .no_follow = true});
+    var walker = try content_dir.walk(arena);
+    defer walker.deinit();
+
+    const allowed_types = [_][]const u8{ ".png", ".tga", ".jpg" };
+    var textures = std.ArrayList(std.fs.Dir.Walker.Entry).init(arena);
+    defer textures.deinit();
+
+    while (try walker.next()) |entry| {
+        const ext = std.fs.path.extension(entry.basename);
+        const is_texture = for (allowed_types) |allowed_type| {
+            if (std.mem.eql(u8, ext, allowed_type)) {
+                break true;
+            }
+        }else false;
+
+        if (is_texture) {
+            const path_length = entry.path.len;
+
+            var metadata_path_buffer: [1024]u8 = undefined;
+            const metadata_path = try std.fmt.bufPrint(&metadata_path_buffer, "../../../content/{s}.texture", .{entry.path[0..(path_length-4)]});
+            var metadata = std.fs.cwd().createFile(metadata_path, .{.exclusive = true}) catch |e| {
+                switch (e) {
+                    error.PathAlreadyExists => {
+                        continue;
+                    },
+                    else => return e,
+                }
+            };
+            defer metadata.close();
+
+            const format = inferrFormatFromFilename(entry.basename);
+
+            _ = try metadata.write("version: 1\n");
+            _ = try metadata.write("source: ");
+
+            _ = try metadata.write(entry.path);
+            _ = try metadata.write("\n");
+
+            _ = try metadata.write("format: ");
+            switch (format) {
+                .BC1_UNORM => {
+                    _ = try metadata.write("BC1_UNORM\n");
+                },
+                .BC1_UNORM_SRGB => {
+                    _ = try metadata.write("BC1_UNORM_SRGB\n");
+                    _ = try metadata.write("srgb: true\n");
+                },
+                .BC3_UNORM => {
+                    _ = try metadata.write("BC3_UNORM\n");
+                },
+                .BC4_UNORM => {
+                    _ = try metadata.write("BC4_UNORM\n");
+                },
+                .BC5_UNORM => {
+                    _ = try metadata.write("BC5_UNORM\n");
+                },
+            }
+            _ = try metadata.write("\n");
+
+
+            std.log.debug("Generated metadata for: {s}", .{entry.basename});
+        }
+    }
+}
+
+fn inferrFormatFromFilename(filename: []const u8) TextureFormat {
+    const fallback_format = TextureFormat.BC1_UNORM_SRGB;
+
+    if (filename.len <= 12) {
+        return fallback_format;
+    }
+
+    var substring = filename[(filename.len - 10)..(filename.len - 4)];
+    if (std.mem.eql(u8, "normal", substring)) {
+        return .BC5_UNORM;
+    }
+
+    if (std.mem.eql(u8, "albedo", substring)) {
+        return .BC1_UNORM_SRGB;
+    }
+
+    if (std.mem.eql(u8, "height", substring)) {
+        return .BC4_UNORM;
+    }
+
+    substring = filename[(filename.len - 12)..(filename.len - 4)];
+    if (std.mem.eql(u8, "emissive", substring)) {
+        return .BC1_UNORM_SRGB;
+    }
+
+    substring = filename[(filename.len - 7)..(filename.len - 4)];
+    if (std.mem.eql(u8, "arm", substring)) {
+        return .BC1_UNORM;
+    }
+
+    return fallback_format;
 }
